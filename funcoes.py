a5a1c47c3f61e9d9b20a1e148560f3d3cc91224c
Wanessa Santos <145464872+WanessaMSantos@users.noreply.github.com>
2024-01-16 15:09:46 -0300

    Add files via upload

---
+++ funcoes.py
@@ -0,0 +1,15 @@
+def calculadora (num1, num2, operacao):
+    if (operacao == 1):
+        return num1 + num2
+    elif (operacao == 2):
+        return num1 - num2
+    elif (operacao == 3):
+        return num1 * num2
+    elif (operacao == 4):
+        return num1 / num2
+
+    else:
+        return 0
+
+resultado = calculadora(2,5,3) 
+    print(resultado)
